57b5ec9f257c71db51dc215dd476b2bf5ec44deb
guadalupecarreno <guadalupe-c@hotmail.com>
2024-01-17 19:14:37 -0300

    Atributos de la tabla materia

--- flowa/database/migrations/2024_01_16_235254_create_materias_table.php
+++ flowa/database/migrations/2024_01_16_235254_create_materias_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
 
-            $table->string('nombre');
+            $table->string('nombre_materia')->unique();
+            $table->bigInteger('codigo')->unique();
             
             $table->unsignedBigInteger('profesor_id');        
             $table->foreign('profesor_id')->references('id')->on('profesors')->onDelete('cascade');
