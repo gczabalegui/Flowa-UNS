6134d1af1a03b106b32b11e1af3a7d3fb308f25c
guadalupecarreno <guadalupe-c@hotmail.com>
2024-01-17 23:46:22 -0300

    Atributos de la tabla plan (a continuar)

--- flowa/database/migrations/2024_01_17_200237_create_plans_table.php
+++ flowa/database/migrations/2024_01_17_200237_create_plans_table.php
@@ -15,8 +15,56 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
 
+            //ENCABEZADO
             $table->unsignedBigInteger('materia_id');
             $table->foreign('materia_id')->references('id')->on('materias')->onDelete('cascade');
+            //Con la materia, podríamos conseguir el código de materia y el departamento al que pertenece (datos que van en el plan)
+
+            //RECUADRO
+            $table->unsignedInteger('anio'); 
+            //Con la materia, podríamos conseguir quien es el profesor (dato que va en el plan)
+            //Carga horaria:
+            $table->unsignedInteger('horas_totales');
+            $table->unsignedInteger('horas_teoricas');
+            $table->unsignedInteger('horas_practicas');
+
+            $table->unsignedInteger('DTE');
+            $table->unsignedInteger('RTF');
+            $table->unsignedInteger('creditos_academicos');
+
+            $table->enum('area_tematica', ['form_basica', 'form_aplicada', 'form_profesional']);
+
+            /*
+            Aprobadas y desaprobadas creo que sería un combo box y cuando trae la materia, también trae el código. 
+            */
+            
+
+            //CUERPO    
+            /*
+            Cuerpo docente creo que también sería un combo box con todos los docentes pero ademas de alguna forma
+            habría que poder escribir la formación de grado, cargo y dedicación de cada uno de los docentes del cuerpo. 
+            */
+
+            $table->text('fundamentacion'); 
+            
+            //Objetivos: 
+                //Generales
+                $table->text('obj_conceptuales'); 
+                $table->text('obj_procedimeentales'); 
+                $table->text('obj_actitudinales'); 
+            $table->text('obj_especificos'); 
+
+            //Contenidos
+            $table->text('cont_minimos'); 
+            $table->text('programa_analitico'); 
+            $table->text('act_practicas'); 
+
+            //Modalidad de enseñanza - aprendizaje
+            $table->text('modalidad'); 
+
+            //Bibliografia
+            $table->text('bibliografia'); 
+
         });
     }
 
