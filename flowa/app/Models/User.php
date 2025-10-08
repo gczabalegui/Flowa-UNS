3344299a680a6e6dcc930f5c109ad9c2d6252cfa
florlosta <florloustaunau@gmail.com>
2025-10-08 18:10:47 -0300

    recuperar contraseña email funcional

--- flowa/app/Models/User.php
+++ flowa/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -52,5 +53,10 @@ class User extends Authenticatable
 public function isUser()
 {
     return $this->role === 'user';
+}
+
+public function sendPasswordResetNotification($token)
+{
+    $this->notify(new ResetPasswordNotification($token));
 }
 }
